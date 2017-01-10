77852b0bf6a422236933b0335402d4bb0fbf96aa
Marcin Kulik <m@ku1ik.com>
2017-01-10 19:10:27 +0100

    Add usage example

--- gen-png.sh
+++ gen-png.sh
@@ -1,7 +1,11 @@
 #!/usr/bin/env bash
 
 # usage:
-# .gen-png.sh path/or/url/to/asciicast.json path/to/output.png seconds-in
+#   ./gen-png.sh path/or/url/to/asciicast.json path/to/output.png seconds-in
+#
+# examples:
+#   ./gen-png.sh https://asciinema.org/a/42383.json output.png 78
+#   ./gen-png.sh demo.json output.png 10.5
 
 phantomjs \
   gen-png.js \
